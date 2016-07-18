9f768af8734d1a7551e200f9da2b6f0b7eb339f2
shootermv <shootermv@gmail.com>
2016-07-18 15:11:00 +0300

    default icon set to be little helper icon

--- devtools.js
+++ devtools.js
@@ -3,5 +3,5 @@
 // chrome.extension.*
 
 // Create a tab in the devtools area
-chrome.devtools.panels.create("Fed Little Helper", "toast.png", "panel.html", function(panel) {});
+chrome.devtools.panels.create("Fed Little Helper", "SantaLittleHelper.png", "panel.html", function(panel) {});
  
