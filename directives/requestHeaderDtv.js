efea13bbd300cf318a05eefae2752f4fb6aee469
urrri <yury.go@gmail.com>
2016-07-19 12:25:24 +0300

    fixed delete button event

--- directives/requestHeaderDtv.js
+++ directives/requestHeaderDtv.js
@@ -1,21 +1,21 @@
 angular.module('devLittlehelper')
-.directive('requestHeader', function(){
+    .directive('requestHeader', function () {
 
-    return {
-      restrict:'E',
-      replace:true,
-      templateUrl:'/directives/requestHeaderTpl.html',
-      scope:{
-         request:'=' ,
-         removeRequest:'&'
-      },
-      link: function(scope, elem ,sttr){
-      // alert()
-        scope.onClick=function($event){
-           $event.stopPropagation();    
-          scope.removeRequest({$event:$event, request:scope.request});
+        return {
+            restrict: 'E',
+            replace: true,
+            templateUrl: 'directives/requestHeaderTpl.html',
+            scope: {
+                request: '=',
+                removeRequest: '&'
+            },
+            link: function (scope, elem, sttr) {
+                // alert()
+                scope.onClick = function ($event) {
+                    $event.stopPropagation();
+                    scope.removeRequest({$event: $event, request: scope.request});
+                }
+            }
+
         }
-      }
-
-    }
-})
+    })
