c07a63f356b98572d56ee7258ac265c277bb65fa
urrri <yury.go@gmail.com>
2016-07-19 14:14:36 +0300

    editing header

--- directives/requestHeaderDtv.js
+++ directives/requestHeaderDtv.js
@@ -10,11 +10,24 @@ angular.module('devLittlehelper')
                 removeRequest: '&'
             },
             link: function (scope, elem, sttr) {
+
+                elem.find('select').material_select();
                 // alert()
-                scope.onClick = function ($event) {
+                scope.onHeaderClick = function ($event) {
+                    if(scope.editing) $event.stopPropagation();
+                };
+                scope.onDelete = function ($event) {
                     $event.stopPropagation();
                     scope.removeRequest({$event: $event, request: scope.request});
-                }
+                };
+                scope.onEdit = function ($event) {
+                    $event.stopPropagation();
+                    scope.editing = true;
+                };
+                scope.onDone = function ($event) {
+                    $event.stopPropagation();
+                    scope.editing = false;
+                };
             }
 
         }
